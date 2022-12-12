9e4c33a0ce4f994a095b8f43e945bde4b3b53a6b
developerpanda083 <cpandalee083@gmail.com>
2022-12-12 01:03:30 -0500

    End of day 12/12/22

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const canvas = document.querySelector('.canvas')
 const canvasSquare = document.querySelector('.canvas-square');
 
+
+function mouseOver () {
+    canvasSquare.style.backgroundColor = "red";
+}
+
+canvasSquare.addEventListener('mouseover',mouseOver);
+
 for (let i = 1; i < 256; i++) {
-    const canvasSqrClone = canvasSquare.cloneNode(true);
+    let canvasSqrClone = canvasSquare.cloneNode(true);
+
+
+
     canvas.appendChild(canvasSqrClone);
+    
 };
+
+
