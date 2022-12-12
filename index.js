b98922d9263ed3afb485b5e4a9282659248f46a7
developerpanda083 <cpandalee083@gmail.com>
2022-12-12 15:43:54 -0500

    Got hover effect over all squares working

--- index.js
+++ index.js
@@ -2,19 +2,17 @@ const canvas = document.querySelector('.canvas')
 const canvasSquare = document.querySelector('.canvas-square');
 
 
-function mouseOver () {
-    canvasSquare.style.backgroundColor = "red";
+function mouseOver (e) {
+    e.target.style.backgroundColor = "red";
 }
-
-canvasSquare.addEventListener('mouseover',mouseOver);
 
 for (let i = 1; i < 256; i++) {
     let canvasSqrClone = canvasSquare.cloneNode(true);
-
-
-
     canvas.appendChild(canvasSqrClone);
-    
 };
 
+const canvasSqrAll = document.querySelectorAll('.canvas-square');
 
+for (let sqr = 1; sqr < canvasSqrAll.length; sqr++) {
+    canvasSqrAll[sqr].addEventListener('mouseover',mouseOver);
+}
